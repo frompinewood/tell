0dfcc2e20801ee2e6d1cf40b9e70fb64b3885b03
Micaiah Parker <frompinewood@icloud.com>
2025-02-07 14:42:22 -0500

    init

---
+++ src/tell.hrl
@@ -0,0 +1,33 @@
+%% based on RFC 854
+
+%% telnet commands 
+-define(SE, 240).
+-define(NOP, 241).
+-define(DM, 242).
+-define(BRK, 243).
+-define(IP, 244).
+-define(AO, 245).
+-define(AYT, 246).
+-define(EC, 247).
+-define(EL, 248).
+-define(GA, 249).
+-define(SB, 250).
+-define(WILL, 251).
+-define(WONT, 252).
+-define(DO, 253).
+-define(DONT, 254).
+-define(IAC, 255).
+
+%% telnet options
+%% RFC 856
+-define(BINARY, 0). 
+%% RFC 857
+-define(ECHO, 1).
+%% RFC 858
+-define(SUPRESS_GO_AHEAD, 3).
+%% RFC 859
+-define(STATUS, 5).
+
+%% Subnegotiation Terms
+-define(IS, 0).
+-define(SEND, 1).
