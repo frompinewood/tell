f4823d439c3714411381b1cbb50df84ac883758f
Micaiah Parker <frompinewood@icloud.com>
2025-02-07 14:41:02 -0500

    incomplete parse command

--- src/tell.erl
+++ src/tell.erl
@@ -1,3 +1,32 @@
 -module(tell).
 
--export([]).
+-include("tell.hrl").
+
+-export([parse/1]).
+
+-spec parse(Data::iodata()) -> {list(tuple()), iodata()}.
+parse(Data) ->
+  parse(Data, [], []).
+
+parse([], Terms, Data) -> {lists:reverse(Terms, lists:reverse(Data))};
+parse([?IAC|Rest], Terms, Data) ->
+  parse_iac(Rest, Terms, Data);
+parse([Byte|Rest], Terms, Data) ->
+  parse(Rest, Terms, [Byte|Data]).
+
+%%% IAC escaped
+parse_iac([?IAC|Rest], Terms, Data) ->
+  parse(Rest, Terms, [?IAC | Data]);
+parse_iac([Command, Option|Rest], Terms, Data) 
+  when Command =:= ?WILL orelse 
+       Command =:= ?WONT orelse 
+       Command =:= ?DO orelse 
+       Command =:= ?DONT ->
+    parse(Rest, [{command_to_atom(Command), option_to_atom(Option)}|Terms], Data).
+
+command_to_atom(_) -> undefined.
+option_to_atom(_) -> undefined.
+  
+
+
+
